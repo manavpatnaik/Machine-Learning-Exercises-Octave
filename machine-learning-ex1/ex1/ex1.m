80ae1b1217ccd1a66adb604bafc250d2751cd96b
Manav Patnaik <manavpatnaik@hotmail.com>
2021-07-09 21:09:58 +0530

    Update ex1.m

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -1,7 +1,7 @@
 %% Machine Learning Online Class - Exercise 1: Linear Regression
 
 %  Instructions
-%  ------------
+%  -----------
 %
 %  This file contains code that helps you get started on the
 %  linear exercise. You will need to complete the following functions
